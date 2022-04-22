6cd86680d7eaadeae9a057c25e7506c2bf51ab34
vladyslav.sharmanov <vladislavvillya@gmail.com>
2022-04-22 22:56:24 +0300

    init

--- node-test/src/config/interfaces/environment.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IEnvironment {
-    [key: string]: string
-}
--- node-test/src/middleware/errorHandlingMiddleware.ts
+++
@@ -1,5 +0,0 @@
-import {NextFunction, Request, Response} from "express";
-
-export function errorHandlingMiddleware(error: Error, req: Request, res: Response): void {
-    res.status(400).send({err: error.message});
-}
